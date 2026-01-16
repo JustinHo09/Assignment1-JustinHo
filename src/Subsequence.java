bbfec6f9a2a3cdf1fe082735e01d9168e837e61d
JustinHo09 <hojustin09@gmail.com>
2026-01-15 20:53:53 -0600

    Added scanner to take input and body for the methods.
    I added the scanner in the body of the first method to take user input, and then I just started on the method header for the subsequence method.

---
+++ src/Subsequence.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Subsequence {
+
+    public static void main(String[] args){
+
+        Scanner scan= new Scanner(System.in);
+        String text1;
+        String text2;
+
+        System.out.println("Please enter the first text:");
+        text1=scan.nextLine();
+        System.out.println("Please enter the second text:");
+        text2=scan.nextLine();
+
+        System.out.println(subsequence(text1, text2));
+    }
+
+    public static int subsequence(String text1, String text2){
+        int result=0;
+
+        return result;
+    }
+}

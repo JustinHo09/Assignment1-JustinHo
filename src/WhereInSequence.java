6786faa90064e92a7a374f41aa241cf1aa26df9d
JustinHo09 <hojustin09@gmail.com>
2026-01-20 14:02:50 -0600

    Added Long and current variables, and implemented case where it find the closest value.
    I added current and next to find the current and next value from not fib. Then I added another section to the if statement where it will check if the target is between current and next, then returns the index of the value its closer to. I also adjusted the output print statement to be more percise

--- src/WhereInSequence.java
+++ src/WhereInSequence.java
@@ -15,14 +15,26 @@ public class WhereInSequence {
         boolean found=false;
         long result=0;
         int counter=1;
+        long current;
+        long next;
         while(!found){
-            if(notFibon.notFib(counter)==target){
+            current=notFibon.notFib(counter);
+            next=notFibon.notFib(counter+1);
+            if(current==target){
                 found=true;
                 result=counter-1;
+            }else if(current<target && target < next){
+                if(target-current < next-target){
+                    found=true;
+                    result=counter-1;
+                }else{
+                    found=true;
+                    result=counter;
+                }
             }
             counter++;
         }
-        System.out.print("The output is: "+result);
+        System.out.print("The closest value is at index: "+result);
         return result;
     }
 }

585be73fd4c61fbc79ca4aa952a91dcf96d3abf9
JustinHo09 <hojustin09@gmail.com>
2026-01-19 20:36:40 -0600

    Added Scanner, prompt, variables and a long array.
    I added a Scanner to take user input in the main method along with a print message telling to user to enter what term of NotFib they want to see until. Then I added some variables I will need for calculating NotFib and added an array to store all of the values.

--- src/NotFibbonacci.java
+++ src/NotFibbonacci.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class NotFibbonacci {
 
     public static void main(String[] args){
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter what term of NotFib you want to see:");
+        int num= scan.nextInt();
+        notFib(num);
 
     }
 
-    public static Long notFib(){
+    public static long[] notFib(int term){
 
-        long result=0;
-
-        return result;
+        long current;
+        long [] terms= new long[term+1];
+        long a=0;
+        long b=2;
+       
+        return terms;
 
     }
 }

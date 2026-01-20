483c6b7303416c00e66b4967edd01509b69d12dd
JustinHo09 <hojustin09@gmail.com>
2026-01-19 21:07:51 -0600

    Added new class WhereInSequence and two methods, main and position.
    This class is made to solve the where in sequence problem, which is to return the index at which an input appears in the NotFibonacci sequence , or returns the index that contains the nearest value to the input. I added the main class to run the position method which will find desired index. I added a NotFibbonacci object to be able to get the sequence. I also added a result variable and return statement to return the result.

---
+++ src/WhereInSequence.java
@@ -0,0 +1,14 @@
+public class WhereInSequence {
+
+    public static void main(String[] args){
+
+    }
+
+    public static int position(){
+        NotFibbonacci notFib= new NotFibbonacci();
+
+        int result=0;
+        
+        return result;
+    }
+}

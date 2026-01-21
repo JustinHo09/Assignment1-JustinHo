06135269aed7dd8dd9e3d1e842787bc82e77ef63
JustinHo09 <hojustin09@gmail.com>
2026-01-20 22:27:23 -0600

    Added scanner and text to prompt input and method call to substring.
    I added the scanner and two string variables in main to take and assign user input into those variables. Then I added print messages to tell the user to enter text. Then I added a method call too substring that takes in user input.

--- src/Substring.java
+++ src/Substring.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Substring {
 
     public static void main (String [] args){
+        Scanner scan= new Scanner(System.in);
+        String text1;
+        String text2;
 
+        System.out.println("Please enter the first text:");
+        text1=scan.nextLine();
+        System.out.println("Please enter the second text:");
+        text2=scan.nextLine();
+        substring(text1,text2);
     }
 
     public static String substring(String text1, String text2){

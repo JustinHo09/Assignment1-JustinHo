72d4831686b6ab61c874dbedc1a642eec8d48413
JustinHo09 <hojustin09@gmail.com>
2026-01-20 19:53:49 -0600

    Created new class Substring and two methods in it main and substring.
    I created the Substring class to solve the substring problem on the assignment which is just to return the longest common substring. I created the main method to eventually take user input for the two texts to compare. The substring method has two parameters which are both strings, that are the two texts to find the longest substring of. Then the substring method had a string declared and initialized, which will be the result. It also has a return statement to return that string.

---
+++ src/Substring.java
@@ -0,0 +1,12 @@
+public class Substring {
+
+    public static void main (String [] args){
+
+    }
+
+    public static String substring(String text1, String text2){
+        String longest="";
+
+        return longest;
+    }
+}

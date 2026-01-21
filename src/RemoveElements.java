b390aec24910e14aaad84bd0dd254bbe4ecda617
JustinHo09 <hojustin09@gmail.com>
2026-01-20 19:21:56 -0600

    Created a new class RemoveElements, created two new methods main and removeElement, added a variable inside of removeElement and a return statement.
    I created a new class RemoveElement that is for the Remove Element problem on the assignment. I also added a main method to take in input. I also added the removeElement method which will have the algorithm to replace the elements. In that method I declared and instantiated a variable k to 0 which will be the output. I also added a return statement to return k.

---
+++ src/RemoveElements.java
@@ -0,0 +1,12 @@
+public class RemoveElements {
+
+    public static void main(String [] args){
+
+    }
+
+    public static int removeElement(double [] nums, double val){
+        int k=0;
+
+        return k;
+    }
+}

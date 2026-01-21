ff90b8e5e3f7f90d1f1b815611acb9fbeafa8759
JustinHo09 <hojustin09@gmail.com>
2026-01-20 19:32:25 -0600

    Added test array and target in main method and added for loop in removeElement method.
    I added an array and target value in the main method to test if the algorithm works. I also added a for loop in the removeElement method. This loop will iterate through the nums array. Then with an if statement if the number at the current iteration is greater than or equal to val then it will replace the num at index k with it. Then increment k.

--- src/RemoveElements.java
+++ src/RemoveElements.java
@@ -1,12 +1,20 @@
 public class RemoveElements {
 
     public static void main(String [] args){
-
+        double[] test= {0,5,8,10,12,54,-9,3,12,-43,345,54,-98,15};
+        removeElement(test,12);
     }
 
     public static int removeElement(double [] nums, double val){
         int k=0;
 
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]>=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        System.out.println("There are : "+k+" elements >= "+val);
         return k;
     }
 }

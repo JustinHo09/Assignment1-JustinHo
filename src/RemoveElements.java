fd58e59a392525f332cb6e41e8b9308bacd26a32
JustinHo09 <hojustin09@gmail.com>
2026-01-22 20:23:27 -0600

    Added print statement for input
    I added a print statement to print the target input and a loop to print all of the inputs for the array.

--- src/RemoveElements.java
+++ src/RemoveElements.java
@@ -4,6 +4,11 @@ public class RemoveElements {
         double[] test= {0,5,8,10,12,54,-9,3,12,-43,345,54,-98,15};
         int target=12;
         int num=removeElement(test,target);
+        String input="Input array: ";
+        for(int i=0;i<test.length;i++){
+            input= input + test[i]+", ";
+        }
+        System.out.println("Input target: "+ target);
         System.out.println("There are : "+num+" elements >= "+target);
     }
 

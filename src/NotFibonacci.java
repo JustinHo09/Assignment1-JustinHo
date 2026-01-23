f66f82f6576117d5a91df1f11338ba4f5c7c11c6
JustinHo09 <hojustin09@gmail.com>
2026-01-22 20:12:07 -0600

    Moved the output print statement to main, removed the terms array and any statements that rely on it.
    I removed the terms array and any statements that relied on it, for space reasons. Then instead of concaintating results for the sequence, I print out the value of b after current is calculated. I then moved a portion of the output print statement to the main method.

--- src/NotFibonacci.java
+++ src/NotFibonacci.java
@@ -7,41 +7,39 @@ public class NotFibonacci {
         System.out.println("Please enter how many terms of the Not Fibonacci " +
                 "sequence you want to see:");
         int num= scan.nextInt();
+        if(num>0) {
+            System.out.print("The NotFibonacci sequence is: ");
+        }
         notFib(num);
 
     }
 
     public static long notFib(int term){
-
         long current=0;
-        long [] terms= new long[term]; //S(n)
         long a=0;
         long b=2;
-        String sequence= "The NotFibonacci sequence is: ";
-        if(term == 1){
-            terms[0]=a;
-            System.out.println(sequence + a+".");
-            return current;
-        }else{
-            terms[0]=a;
-            terms[1]=b;
+        if(term <=0){
+            System.out.println("Not a valid input try again");
+            return 0;
         }
+        if(term == 1){
+            System.out.print(a);
+            return current;
+        }
+        System.out.print(a);
+        System.out.print(", ");
+        System.out.print(b);
+        System.out.print(", ");
         if(term > 2){
             for( int i=2; i<term; i++){
                 current= ((3*b)/2) + (2*a);
-                terms[i] = current;
                 a=b;
                 b=current;
+                System.out.print(b);
+                System.out.print(", ");
             }
         }
 
-        for(int j=0;j<term;j++){
-
-            sequence= sequence + terms[j] + ", ";
-
-        }
-
-        System.out.println(sequence);
         return current;
 
     }

f586ece2320dea8e6fe0cfecf2e97730ea5d938f
JustinHo09 <hojustin09@gmail.com>
2026-01-22 19:58:56 -0600

    Move the output print to main.
    I moved the print statement for the output which was originally in the subsequence method to the main method.

--- src/Subsequence.java
+++ src/Subsequence.java
@@ -12,16 +12,15 @@ public class Subsequence {
         text1=scan.nextLine();
         System.out.println("Please enter the second text:");
         text2=scan.nextLine();
-
-        System.out.println(subsequence(text1, text2));
+        System.out.println("Output: " +subsequence(text1,text2));
     }
 
     public static int subsequence(String text1, String text2){
 
-        int result=0;
+        int result=0; 
         char [] small;
         char [] big;
-
+        int start;
         if(text1.length()<=text2.length()){
             small=text1.toCharArray();
             big=text2.toCharArray();
@@ -32,9 +31,10 @@ public class Subsequence {
 
         int [] sizes= new int[small.length];
 
+
         for(int k=0; k<small.length;k++){
             result=0;
-            int start = 0;
+            start = 0;
             for (int i = k; i < small.length; i++) {
                 for (int j = start; j < big.length; j++) {
                     if (small [i] == big [j]) {
